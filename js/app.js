4afe9def797aba0c52b0601ed70ac2d633718411
nyrta1 <nurtai_2005@mail.ru>
2025-11-12 14:41:18 +0500

    gew

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 
-import { disablePageZoom } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/app.nozoom.js";
-import { renderCameraGrid } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.grid.js";
-import { enableCameraDrag } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.drag.js";
-import { enableCameraExpand } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.expand.js";
-import { enableCameraSound } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.sound.js";
-import { enableTimelineController } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.timeline.controller.js";
-import { enableCameraSpeed } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.speed.js";
-import { enableCameraScreenshot } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.screenshot.js";
-import { enableCameraPlayPause } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.playpause.js";
-import { enableCameraZoom } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.zoom.js";
-import { enableTimelinePreview } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/master/js/camera.timeline.preview.js";
+import { disablePageZoom } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/app.nozoom.js";
+import { renderCameraGrid } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.grid.js";
+import { enableCameraDrag } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.drag.js";
+import { enableCameraExpand } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.expand.js";
+import { enableCameraSound } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.sound.js";
+import { enableTimelineController } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.timeline.controller.js";
+import { enableCameraSpeed } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.speed.js";
+import { enableCameraScreenshot } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.screenshot.js";
+import { enableCameraPlayPause } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.playpause.js";
+import { enableCameraZoom } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.zoom.js";
+import { enableTimelinePreview } from "https://raw.githubusercontent.com/nyrta1/stc-vp-core/refs/heads/v1/js/camera.timeline.preview.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   disablePageZoom();
